50247603c1fe1f0bd718cbebc60c87c9252346fa
David Thomas <dmthomas003@gmail.com>
2019-03-24 12:05:21 -0400

    minor changes to urls.py

--- egscripts/urls.py
+++ egscripts/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^library/', include('library.urls')),
+    #url(r'^library/', include('library.urls')),
     url(r'', include('library.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     ]
